1e9386df87c1a4d2957ff85d7a9fbffd6f58c5a4
Jumaniyaz <zumaniazkozubaev@gmail.com>
2025-05-10 14:11:46 +0600

    jumaniyaz

--- src/component/New/News.jsx
+++ src/component/New/News.jsx
@@ -3,36 +3,36 @@ import axios from 'axios';
 import './News.scss';
 
 function News() {
-  const [news, setNews] = useState([]);  
-  const [loading, setLoading] = useState(true);  
-  const [error, setError] = useState(null);  
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     // Бекендден маалымат алуу
-    axios.get('http://ec2-51-21-3-88.eu-north-1.compute.amazonaws.com/api/news') 
+    axios.get('http://ec2-51-21-3-88.eu-north-1.compute.amazonaws.com/api/news')
       .then(response => {
-        setNews(response.data); 
-        setLoading(false); 
+        setNews(response.data);
+        setLoading(false);
       })
       .catch(err => {
-        setError(err.message);  
-        setLoading(false);  
+        setError(err.message);
+        setLoading(false);
       });
-  }, []); 
+  }, []);
 
   if (loading) {
-    return <div>Loading...</div>;  
+    return <div>Loading...</div>;
   }
 
   if (error) {
-    return <div>Error: {error}</div>;  
+    return <div>Error: {error}</div>;
   }
 
   return (
     <div className='news'>
       {news.map((item) => (
         <div className='content' key={item.id}>
-        <img src={`http://ec2-51-21-3-88.eu-north-1.compute.amazonaws.com${item.images}`} alt={item.name} />
+          <img src={`http://ec2-51-21-3-88.eu-north-1.compute.amazonaws.com${item.images}`} alt={item.name} />
           <div className='content-text'>
             <h6>{item.price}</h6>
             <h5>{item.name}</h5>
